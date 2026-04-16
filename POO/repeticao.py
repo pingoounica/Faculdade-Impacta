00d1bff9bf6596fe3cf4f1cc670ac22d77004495
Vanessa Pires de Almeida <98252042+pingoounica@users.noreply.github.com>
2026-04-16 15:12:39 -0300

    Delete POO directory

--- POO/repeticao.py
+++
@@ -1,9 +0,0 @@
-n = int(input('digite um numero: '))
-
-cont = 0
-while cont < n:
-    print(cont)
-    cont += 1
-
-for cont2 in range(n):
-    print(cont2)
--- POO/selecao.py
+++
@@ -1,8 +0,0 @@
-n = int(input('digite um numero: '))
-
-if n > 10:
-    print(f'{n} é maior que 10')
-elif n == 10:
-    print(f'{n} é igual a 10')
-else:
-    print(f'{n} é menor a 10')
